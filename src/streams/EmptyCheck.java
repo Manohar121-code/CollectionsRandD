67c89558ec50dcdd04f01aa4e044801ab83bad9b
Manohar51 <manohar.glimpse@gmail.com>
2018-12-05 16:09:04 +0530

    Commit in streams

---
+++ src/streams/EmptyCheck.java
@@ -0,0 +1,13 @@
+package streams;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class EmptyCheck {
+	public static void main(String[] args) {
+		List<String> list = Arrays.asList("Manohar", "", "Sambali", "Hello", "World", "", null);
+		List<String> filtered = list.stream().filter(s -> s != null && (!s.isEmpty() || !s.equals("Manohar"))).collect(Collectors.toList());
+		System.out.println(filtered);
+	}
+}
